54cc749463389ce02ba54c0a6edb7ecd4a493119
bryan <dilauraba@gmail.com>
2016-04-08 11:01:57 -0600

    uncommented lru stuff.

--- src/classes/LRU.cpp
+++ src/classes/LRU.cpp
@@ -7,62 +7,62 @@
 
 #include "LRU.h"
 
-//LRU::LRU(unsigned int num_ways) {
-//	// TODO create a data structure for the buffer
-//    if(num_ways <= 0){
-//        printf("Invalid number of ways given");
-//        return
-//    }
-//    else if(num_ways == 1){
-//        printf("Theres really no point in having an LRU for a 1 way cache");
-//        return
-//    }
-//    else{
-//        headt = new LruNode;
-//        headt.way = 1;
-//        headt.next = NULL;
-//        LruNode* last = &head;
-//        for(i = 2; i <= num_ways; ++i){
-//            tmp = new LruNode;
-//            tmp.way = i;
-//            tmp.next = NULL;
-//            *last.next = &tmp;
-//            last = &tmp;
-//        }
-//    }
-//
-//}
-//
-//LRU::~LRU() {
-//	// TODO Auto-generated destructor stub
-//    LruNode* ahead = &head;
-//    while(ahead != NULL){
-//        LruNode* current = ahead;
-//        ahead = current.next;
-//        delete current;
-//    }
-//}
-//
-//LRU::void update(unsigned int t_way){
-//    LruNode* back = NULL;
-//    LruNode* ahead = NULL;
-//    LruNode* curr = head;
-//    while(*curr.way != t_way){
-//
-//    }
-//    if(curr == &head){
-//        //The buffer is alread correct
-//        return;
-//    }
-//    else{
-//        *back.next = ahead;
-//        *curr.next = head;
-//        head = curr;
-//    }
-//
-//}
-//
-//LRU::unsigned int fetch(){
-//
-//}
+LRU::LRU(unsigned int num_ways) {
+	// TODO create a data structure for the buffer
+    if(num_ways <= 0){
+        printf("Invalid number of ways given");
+        return
+    }
+    else if(num_ways == 1){
+        printf("Theres really no point in having an LRU for a 1 way cache");
+        return
+    }
+    else{
+        headt = new LruNode;
+        headt.way = 1;
+        headt.next = NULL;
+        LruNode* last = &head;
+        for(i = 2; i <= num_ways; ++i){
+            tmp = new LruNode;
+            tmp.way = i;
+            tmp.next = NULL;
+            *last.next = &tmp;
+            last = &tmp;
+        }
+    }
+
+}
+
+LRU::~LRU() {
+	// TODO Auto-generated destructor stub
+    LruNode* ahead = &head;
+    while(ahead != NULL){
+        LruNode* current = ahead;
+        ahead = current.next;
+        delete current;
+    }
+}
+
+LRU::void update(unsigned int t_way){
+    LruNode* back = NULL;
+    LruNode* ahead = NULL;
+    LruNode* curr = head;
+    while(*curr.way != t_way){
+
+    }
+    if(curr == &head){
+        //The buffer is alread correct
+        return;
+    }
+    else{
+        *back.next = ahead;
+        *curr.next = head;
+        head = curr;
+    }
+
+}
+
+LRU::unsigned int fetch(){
+
+}
 
--- src/classes/LRU.h
+++ src/classes/LRU.h
@@ -5,22 +5,22 @@
  *      Author: bryan
  */
 
-//#ifndef LRU_H_
-//#define LRU_H_
-//
-//class LRU {
-//private:
-//    LruNode * head;
-//public:
-//	LRU();
-//	virtual ~LRU(unsigned int num_ways);
-//    void update(unsigned int t_way);
-//    unsigned int fetch();
-//};
-//
-//struct LruNode{
-//    unsigned int way;
-//    LruNode * next;
-//}
-//
-//#endif /* LRU_H_ */
+#ifndef LRU_H_
+#define LRU_H_
+
+class LRU {
+private:
+    LruNode * head;
+public:
+	LRU();
+	virtual ~LRU(unsigned int num_ways);
+    void update(unsigned int t_way);
+    unsigned int fetch();
+};
+
+struct LruNode{
+    unsigned int way;
+    LruNode * next;
+}
+
+#endif /* LRU_H_ */
