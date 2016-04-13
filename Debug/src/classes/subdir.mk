d28d45478f5025e9f60ecca49a86e6174256d803
bryan <dilauraba@gmail.com>
2016-04-12 21:33:42 -0600

    Added make stuff?

---
+++ Debug/src/classes/subdir.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/classes/Cache.cpp \
+../src/classes/CacheSystem.cpp \
+../src/classes/Dictionary.cpp \
+../src/classes/LRU.cpp \
+../src/classes/Memory.cpp \
+../src/classes/Victim.cpp \
+../src/classes/Way.cpp \
+../src/classes/conf_parser.cpp 
+
+OBJS += \
+./src/classes/Cache.o \
+./src/classes/CacheSystem.o \
+./src/classes/Dictionary.o \
+./src/classes/LRU.o \
+./src/classes/Memory.o \
+./src/classes/Victim.o \
+./src/classes/Way.o \
+./src/classes/conf_parser.o 
+
+CPP_DEPS += \
+./src/classes/Cache.d \
+./src/classes/CacheSystem.d \
+./src/classes/Dictionary.d \
+./src/classes/LRU.d \
+./src/classes/Memory.d \
+./src/classes/Victim.d \
+./src/classes/Way.d \
+./src/classes/conf_parser.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/classes/%.o: ../src/classes/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
