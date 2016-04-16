feecab0cf18809028b255d214f174c60809f614c
erik-feller <erik.feller@colorado.edu>
2016-04-16 10:42:29 +0000

    Building the victim class

--- src/classes/Victim.cpp
+++ src/classes/Victim.cpp
@@ -6,13 +6,55 @@
  */
 
 #include "Victim.h"
+#include <cstddef>
 
 Victim::Victim() {
 	// TODO Auto-generated constructor stub
+    VicNode* thead = new VicNode;
+    thead->next = NULL;
+    thead->address = 0;
+    thead->dirty = true;
+    VicNode* curr = thead;
+    for(int i = 0; i < 7; ++i){
+        curr->next = new VicNode;
+        curr = curr->next;
+        curr->next = NULL;
+        curr->address = 0;
+        curr->dirty = true;
+    }
+    this->head = thead;
+}
 
+bool Victim::check(unsigned long long int addr){
+    // TODO add code to check for an address
+    VicNode* curr = this->head;
+    while(curr->address != addr){
+       //check to see if at the end of viccache
+        
+    }
+}
+
+bool Victim::swap(unsigned long long int oldAddr, unsigned long long int newAddr){
+    //TODO add code to swap two address 
+}
+
+bool Victim::push(unsigned long long int newAddr){
+    //TODO delete the old item add the new one
+}
+
+void Victim::reorder(unsigned long long int target){
+    //TODO place the targe on top of the victim cache
 }
 
 Victim::~Victim() {
 	// TODO Auto-generated destructor stub
+    VicNode* curr = this->head;
+    VicNode* ahead = curr->next;
+    while(ahead != NULL){
+        delete curr;
+        curr = ahead;
+        ahead = curr->next;
+    }
+    delete curr;
 }
 
--- src/classes/Victim.h
+++ src/classes/Victim.h
@@ -10,27 +10,36 @@
 
 #include "Dictionary.h"
 
+struct VicNode{
+    unsigned long long int address;
+    VicNode* next;
+    bool dirty;
+};
+
 class Victim{
 public:
 	Victim();
 	//returns if an item is present in the victim cache, and the location (-1 if not in, otherwise [0-7]
-	int check(unsigned long long int address);
+	bool check(unsigned long long int addr);
 	//replace an entry in the victim cache
-	void replace(int location, unsigned long long int newAddr);
-
-
+	bool swap(unsigned long long int oldAddr, unsigned long long int newAddr);
+    //Add item to the chache. Kickout the least recent member
+    bool push(unsigned long long int newAddr);
+    //destructor
 	virtual ~Victim();
 
 
 private:
-
+    
+    VicNode* head;
 	//dirty kickout
+    //re-order
+    void reorder(unsigned long long int target);
+    //check for addr
 
 	//i dont know...
 
 
-	//the entries of the victim cache
-	unsigned long long int entries[8];
 
 };
 
